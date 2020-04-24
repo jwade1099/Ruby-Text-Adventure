0369493a04ef9cf69ddf278c6776df5334b33b86
jwade1099 <43207092+jwade1099@users.noreply.github.com>
2020-04-23 21:56:14 -0400

    Create Game.rb

---
+++ Game.rb
@@ -0,0 +1,40 @@
+#Justin Wade 4/21/20
+require_relative  'test'
+
+#-------------------------------------------------------------------------------------------------------------------------------------------
+#functions
+
+def function1 (value)
+  return value+2
+end
+#-------------------------------------------------------------------------------------------------------------------------------------------
+#
+count = 0
+health = 100
+
+#-------------------------------------------------------------------------------------------------------------------------------------------
+#food
+apple = 30
+pumpkinpie = 70
+
+#-------------------------------------------------------------------------------------------------------------------------------------------
+puts "Welcome to this game. Before we start I need to ask some questions."
+puts "Are you a boy or a girl?"
+
+class Planet
+
+  def initialize(wood, isToxic, iron, cows, sheeps)
+  @wood = wood
+  @isToxic = isToxic
+  @iron = iron
+  @cows = cows
+  @sheep = sheeps
+  end
+
+
+
+end
+
+
+xeeb = Planet.new(120,21,100, 20, 23)
+puts xeeb.wood
